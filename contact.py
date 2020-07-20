5ce49c5c1f211be39b96e93c2debf2a06a5eef5f
khizha <khizhnyakanna@gmail.com>
2020-07-20 19:57:34 +0300

    Contact class added

--- contact.py
+++ contact.py
@@ -1,6 +1,27 @@
 class Contact:
 
-    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, home, mobile, work, fax, email, email2, email3, homepage, bday, bmonth, byear, aday, amonth, ayear, address2, phone2, notes):
-        self.name=name
-        self.header=header
-        self.footer=footer
+    def __init__(self, firstname, middlename, lastname, nickname, title, company, address, homeaddress, mobilephone, workphone, fax, email, email2, email3, homepage, birthday, birthmonth, birthyear, aday, amonth, ayear, address2, phone2, notes):
+        self.firstname=firstname
+        self.middlename=middlename
+        self.lastname=lastname
+        self.nickname=nickname
+        self.title=title
+        self.company=company
+        self.address=address
+        self.homeaddress=homeaddress
+        self.mobilephone=mobilephone
+        self.workphone=workphone
+        self.fax=fax
+        self.email=email
+        self.email2=email2
+        self.email3=email3
+        self.homepage=homepage
+        self.birthday=birthday
+        self.birthmonth=birthmonth
+        self.birthyear=birthyear
+        self.aday=aday
+        self.amonth=amonth
+        self.ayear=ayear
+        self.address2=address2
+        self.phone2=phone2
+        self.notes=notes
