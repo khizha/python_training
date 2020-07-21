d7b8304ede7b798a1bd405405ed525a3af9fed6a
khizha <khizhnyakanna@gmail.com>
2020-07-21 22:37:33 +0300

    1) test_add_contact.py file moved to "test" python package 2) contacts helper class created and incorporated into the code 3) code changed to use Application fixture

---
+++ test/test_add_contact.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from model.contact import Contact
+from fixture.application import Application
+import pytest
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_contact(app):
+     app.session.login(username="admin", password="secret")
+     app.contact.open_new_contact_page()
+     app.contact.fill_in_new_contact_data(Contact(firstname="fff", middlename="mmm", lastname="lll", nickname="kkk", title="ttt",
+                                                  company="ccc", address="aaa", homeaddress="hhh", mobilephone="5555555",
+                                                  workphone="5555555", fax="5555555", email="f@tt.tt",
+                                                  email2="f2@tt.tt", email3="f3@tt.tt", homepage="http://home.tt", birthday="12",
+                                                  birthmonth="January", birthyear="1999", aday="14", amonth="February", ayear="1987", address2="ffffffffffffffffffffffff",
+                                                  phone2="5555555", notes="note"))
+     app.contact.submit_created_contact()
+     app.session.logout()
