e1be5a24f872556aa8659aec9ab026e7bd906676
khizha <khizhnyakanna@gmail.com>
2020-07-22 23:09:17 +0300

    1) fixture moved to conftest.py file 2) delete first group test added 3) scope = "session" parameter added to fixture

---
+++ conftest.py
@@ -0,0 +1,12 @@
+
+from fixture.application import Application
+import pytest
+
+#@pytest.fixture()
+@pytest.fixture(scope = "session")
+
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
