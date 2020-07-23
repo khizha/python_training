0c184536cc57b00f12db59382c22bab651423e6c
khizha <khizhnyakanna@gmail.com>
2020-07-23 17:07:13 +0300

    Groups update process improved: separate group parameters can be changed now.

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from model.group import Group
 
-def test_modify_group(app):
+#def test_modify_group(app):
+#    app.session.login(username="admin", password="secret")
+#    app.group.modify(Group(name="asd_upd", header="asd_upd", footer="asd_upd"))
+#    app.session.logout()
+
+def test_modify_group_name(app):
     app.session.login(username="admin", password="secret")
-    app.group.modify(Group(name="asd_upd", header="asd_upd", footer="asd_upd"))
+    app.group.modify_first_group(Group(name="New Group"))
     app.session.logout()
 
+def test_modify_group_header(app):
+    app.session.login(username="admin", password="secret")
+    app.group.modify_first_group(Group(header="New header"))
+    app.session.logout()
