bd7047424c6ade9044f7e8a4eb8435edc67e979d
khizha <khizhnyakanna@gmail.com>
2020-07-23 22:05:17 +0300

    open_groups_page method optimized: now we check if we are already on Groups page before trying to open the page unconditionally

--- fixture/group.py
+++ fixture/group.py
@@ -71,7 +71,10 @@ class GroupHelper:
 
     def open_groups_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("groups").click()
+        # if the current page address ends with "\group.php" and there is a button with name "new" on this page then
+        # this is a correct page
+        if not (wd.current_url.endswith("/group.php") and len(wd.find_elements_by_name("new")) > 0):
+            wd.find_element_by_link_text("groups").click()
 
     def count(self):
         wd = self.app.wd
