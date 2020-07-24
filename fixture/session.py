db1c2fd8d597f00506f5a8e2cdb8c4697ffe9e12
khizha <khizhnyakanna@gmail.com>
2020-07-24 19:24:50 +0300

    return to contacts==home page (self.app.open_home_page()) addeed into contacts count method

--- fixture/session.py
+++ fixture/session.py
@@ -17,4 +17,5 @@ class SessionHelper:
     def logout(self):
         wd = self.app.wd
         wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_name("user")
         wd.implicitly_wait(3)
