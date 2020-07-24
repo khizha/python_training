4266efdc0cc8aa1d09f2254cdc22df853be258f7
khizha <khizhnyakanna@gmail.com>
2020-07-25 02:14:12 +0300

    groups lists creation implemented

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,8 +2,17 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups=app.group.get_group_list()
     app.group.create(Group(name="asd", header="asd", footer="asd"))
+    new_groups = app.group.get_group_list()
+    # check that the new groups list is 1 element longer than the old lis
+    assert len(old_groups) + 1 == len(new_groups)
     #app.session.logout()
 
 def test_add_empty_group(app):
+    old_groups = app.group.get_group_list()
     app.group.create(Group(name="", header="", footer=""))
+    new_groups = app.group.get_group_list()
+    # check that the new groups list is 1 element longer than the old lis
+    assert len(old_groups) + 1 == len(new_groups)
+
