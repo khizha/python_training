e04e5a1d2345d94757240ba875be666c841c739c
khizha <khizhnyakanna@gmail.com>
2020-08-11 21:32:03 +0300

    caching and hashing implemented for add group method

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,21 +5,21 @@ def test_add_group(app):
     old_groups=app.group.get_group_list()
     group = Group(name="asd", header="asd", footer="asd")
     app.group.create(group)
-    new_groups = app.group.get_group_list()
     # check that the new groups list is 1 element longer than the old lis
-    assert len(old_groups) + 1 == len(new_groups)
+    assert len(old_groups) + 1 == app.group.count()
+    new_groups = app.group.get_group_list()
     old_groups.append(group)
     assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
     #app.session.logout()
 
-def test_add_empty_group(app):
-    old_groups = app.group.get_group_list()
-    group = Group(name="", header="", footer="")
-    app.group.create(group)
-    new_groups = app.group.get_group_list()
-    # check that the new groups list is 1 element longer than the old lis
-    assert len(old_groups) + 1 == len(new_groups)
-    old_groups.append(group)
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+#def test_add_empty_group(app):
+#    old_groups = app.group.get_group_list()
+#    group = Group(name="", header="", footer="")
+#    app.group.create(group)
+#    new_groups = app.group.get_group_list()
+#    # check that the new groups list is 1 element longer than the old lis
+#    assert len(old_groups) + 1 == len(new_groups)
+#    old_groups.append(group)
+#    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
