df7246815a34e000f846e9a00cfc2cdb915521fb
khizha <khizhnyakanna@gmail.com>
2020-08-12 14:15:38 +0300

    contact_cache cache variable added hashing implemented hashing implemented: assert len(old_contacts) -1 == len(new_contacts) changed to assert len(old_contacts) -1 == app.group.count()

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -7,8 +7,8 @@ def test_delete_first_contact(app):
         old_contacts = app.contact.get_contacts_list()
         app.contact.delete_first_contact()
         time.sleep(3)
+        assert len(old_contacts) - 1 == app.contact.count()
         new_contacts = app.contact.get_contacts_list()
-        assert len(old_contacts) - 1 == len(new_contacts)
         # remove first element from the old list of contacts
         old_contacts[0:1] = []
         assert old_contacts == new_contacts
