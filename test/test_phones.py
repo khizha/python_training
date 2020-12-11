ca451c605579c14d05d3fff2d70fab29fc0302d1
khizha <khizhnyakanna@gmail.com>
2020-12-11 13:10:53 +0300

    test_phones_on_contact_view_page updated: backwards validation implemented

--- test/test_phones.py
+++ test/test_phones.py
@@ -9,10 +9,7 @@ def test_phones_on_homepage(app):
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contacts_from_view_page(0)
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
-    assert contact_from_view_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_view_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_view_page.mobilephone == contact_from_edit_page.mobilephone
-    assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
+    assert merge_phones_like_on_homepage(contact_from_view_page) == merge_phones_like_on_homepage(contact_from_edit_page)
 
 def clear(s):
     # remove the following symbols from the s string: "()", " ", "-"
