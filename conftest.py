a93c07dfa09fd6a172e59320bf633f5d5f69d18d
khizha <khizhnyakanna@gmail.com>
2020-12-17 17:29:43 +0300

    groups data comparison from web ond db implemented

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ import json
 import os.path
 import importlib
 import jsonpickle
+from fixture.db import DbFixture
 
 # global variable containing fixture
 fixture = None
@@ -12,34 +13,48 @@ fixture = None
 # global variable containing configuration file data
 target = None
 
+def load_config(file):
+    global target
+
+    if target is None:
+        # get path to the configuration file
+        #if request.config.getoption("--targetpath") is None:
+            # if the path to the configuration file is not provided in the command line then it is located in the current directory
+        config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), file)
+        #else:
+            # if the path to the configuration file is provided
+        #   config_file = os.path.join(request.config.getoption("--targetpath"), file)
+
+        with open(config_file) as f:
+            target = json.load(f)
+
+    return target
+
 @pytest.fixture()
 #@pytest.fixture(scope = "session")
 
 def app(request):
     # use the global variable "fixture"
     global fixture
-    global target
 
     browser = request.config.getoption("--browser")
 
-    if target is None:
-        # get path to the configuration file
-        if request.config.getoption("--targetpath") is None:
-            # if the path to the configuration file is not provided in the command line then it is located in the current directory
-            config_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), request.config.getoption("--target"))
-        else:
-            # if the path to the configuration file is provided
-            config_file = os.path.join(request.config.getoption("--targetpath"), request.config.getoption("--target"))
-
-        with open(config_file) as f:
-            target = json.load(f)
+    web_config = load_config(request.config.getoption("--target"))['web']
 
     # check if fixture does not exist or is corrupted/invalid
     if fixture is None or not fixture.is_valid():
-        fixture = Application(browser=browser, base_url=target['baseUrl'])
-    fixture.session.ensure_login(username=target['username'], password=target['password'])
+        fixture = Application(browser=browser, base_url=web_config['baseUrl'])
+    fixture.session.ensure_login(username=web_config['username'], password=web_config['password'])
     return fixture
 
+@pytest.fixture(scope="session")
+def db(request):
+    db_config = load_config(request.config.getoption("--target"))['db']
+    dbfixture = DbFixture(host=db_config['host'], name=db_config['name'], user=db_config['user'], password=db_config['password'])
+    def fin():
+        dbfixture.destroy()
+    request.addfinalizer(fin)
+    return dbfixture
 
 @pytest.fixture(scope="session", autouse=True)
 def stop(request):
