3f88cd089334909aee3b76aecf1119a65d391433
khizha <khizhnyakanna@gmail.com>
2020-12-21 18:44:12 +0300

    test_add_contact.py: ui/db comparisons and --check_ui switch added

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
 
-def test_add_contact(app, json_contacts):
+def test_add_contact(app, db, json_contacts, check_ui):
     contact = json_contacts
-    old_contacts = app.contact.get_contacts_list()
+    #old_contacts = app.contact.get_contacts_list()
+    old_contacts = db.get_contact_list()
 
     app.contact.open_new_contact_page()
     app.contact.fill_in_new_contact_data(contact)
@@ -11,7 +12,8 @@ def test_add_contact(app, json_contacts):
 
      # check that the new contacts list is 1 element longer than the old list
     assert len(old_contacts) + 1 == app.contact.count()
-    new_contacts = app.contact.get_contacts_list()
+    #new_contacts = app.contact.get_contacts_list()
+    new_contacts = db.get_contact_list()
 
     max_ = new_contacts[0].id
     for i in new_contacts:
@@ -22,3 +24,6 @@ def test_add_contact(app, json_contacts):
     old_contacts.append(contact)
 
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
+    if check_ui:
+        assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contacts_list(), key=Contact.id_or_max)
