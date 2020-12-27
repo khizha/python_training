4a29b4e44fbde3f9b72f09a4d30950bcbf369df6
khizha <khizhnyakanna@gmail.com>
2020-12-27 23:58:03 +0300

    post-test verification added into test_delete_contact_from_group.py test

--- test/test_delete_contact_from_group.py
+++ test/test_delete_contact_from_group.py
@@ -19,7 +19,12 @@ def test_delete_contact_from_group(app, db):
     # get the old list of groups from the DB
     groups_list = db.get_group_list()
     db2 = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+
+    # before the contact is added: number of contacts in the group where the new contact will be added
+    old_contacts_in_group_list = []
+
     for gr in groups_list:
+        old_contacts_in_group_list = db.get_contacts_for_current_group(gr.id)
         contacts_2_delete = db2.get_contacts_in_group(gr)
         if len(contacts_2_delete)>0:
             # delete random contact from contacts_2_delete list from gr group
@@ -36,3 +41,11 @@ def test_delete_contact_from_group(app, db):
 
         # and now delete the added contact from the group
         app.contact.delete_contact_from_group(random_contact.id, gr.name, gr.id)
+
+    # after the contact is deleted: number of contacts in the group where the contact is deleted
+    new_contacts_in_group_list = db.get_contacts_for_current_group(gr.id)
+
+    assert len(new_contacts_in_group_list) == len(old_contacts_in_group_list) - 1
+
+    # check that the selected contact is not in the selected group
+    assert db.contact_is_in_group(random_contact.id, gr.id) == False
